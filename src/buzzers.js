efe1b96758aafe02fcbcf37479c76d986faefaac
Hans Huebner <hans.huebner@gmail.com>
2011-06-21 19:29:01 +0200

    update buzzers so that multiple controllers can be used

--- src/buzzers.js
+++ src/buzzers.js
@@ -1,43 +1,66 @@
 // Simple interface to Sony Buzz! wireless buzzers
 
+var util = require('util');
+var events = require('events');
 var HID = require('HID');
 
 // buzzer protocol info: http://www.developerfusion.com/article/84338/making-usb-c-friendly/
 
-var buzzers = new HID.HID(0x54c, 0x1000);
+function BuzzerController(index)
+{
+    if (!arguments.length) {
+        index = 0;
+    }
 
-buzzers.oldBits = 0;
-buzzers.leds = [0, 0, 0, 0, 0, 0];
+    var controllers = HID.devices(0x54c, 0x1000);
 
-function handleBuzzer(buzzerNumber, bits)
+    if (!controllers.length) {
+        throw new Error("No Buzzer controllers could be found");
+    }
+
+    if (index > controllers.length || index < 0) {
+        throw new Error("Index " + index + " out of range, only " + controllers.length + " Buzzer controllers found");
+    }
+
+    events.EventEmitter.call(this);
+
+    this.hid = new HID.HID(controllers[index].path);
+    this.oldBits = 0;
+    this.leds = [0, 0, 0, 0, 0, 0];
+
+    // Initialize buzzers
+    this.hid.write([0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    this.hid.read(this.buzzerData.bind(this));
+}
+
+util.inherits(BuzzerController, events.EventEmitter);
+
+BuzzerController.prototype.handleBuzzer = function (buzzerNumber, bits)
 {
     var mask = 1 << (buzzerNumber * 5);
     for (var buttonNumber = 0; buttonNumber < 5; buttonNumber++) {
         var now = bits & mask;
-        var old = buzzers.oldBits & mask;
+        var old = this.oldBits & mask;
         if (old ^ now) {
-            buzzers.emit('button', buzzerNumber, buttonNumber, now ? true : false);
+            this.emit('button', buzzerNumber, buttonNumber, now ? true : false);
         }
         mask <<= 1;
     }
 }
 
-function buzzerData (error, data) {
+BuzzerController.prototype.buzzerData = function (error, data) {
+    console.log('error', error, 'data', data);
     var bits = (data[4] << 16) | (data[3] << 8) | data[2];
     for (var i = 0; i < 4; i++) {
-        handleBuzzer(i, bits);
+        this.handleBuzzer(i, bits);
     }
-    buzzers.oldBits = bits;
-    buzzers.read(arguments.callee);
+    this.oldBits = bits;
+    this.hid.read(this.buzzerData.bind(this));
 }
 
-buzzers.led = function(buzzer, state) {
-    buzzers.leds[buzzer + 2] = state ? 0xff : 0x00;
-    buzzers.write(buzzers.leds);
+BuzzerController.prototype.led = function(buzzer, state) {
+    this.leds[buzzer + 2] = state ? 0xff : 0x00;
+    this.hid.write(this.leds);
 }
 
-// Initialize buzzers
-buzzers.write([0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
-buzzers.read(buzzerData);
-
-exports.buzzers = buzzers;
+exports.BuzzerController = BuzzerController;
--- src/test-buzzers.js
+++ src/test-buzzers.js
@@ -1,15 +1,17 @@
 
-buzzers = require('./buzzers').buzzers;
+var buzzers = require('./buzzers');
+
+var buzzerController = new buzzers.BuzzerController();
 
 function blinkLed(buzzerNumber)
 {
-    buzzers.led(buzzerNumber, true);
+    buzzerController.led(buzzerNumber, true);
     setTimeout(function () {
-        buzzers.led(buzzerNumber, false);
+        buzzerController.led(buzzerNumber, false);
     }, 500);
 }
 
-buzzers.on('button', function (buzzer, button, state) {
+buzzerController.on('button', function (buzzer, button, state) {
     console.log('buzzer', buzzer, 'button', button, 'state', state);
     if (state) {
         blinkLed(buzzer);
