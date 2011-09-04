a794e454c4eca70073d39463f48652d3b67615c4
Hans Huebner <hans.huebner@gmail.com>
2011-09-04 08:52:22 +0200

    fix callback arguments

--- src/test-ps3.js
+++ src/test-ps3.js
@@ -6,7 +6,7 @@ var hid = new HID.HID(1356, 616);
 
 console.log('features', hid.getFeatureReport(0xf2, 17));
 
-hid.gotData = function (data) {
+hid.gotData = function (err, data) {
     console.log('got ps3 data', data);
     this.read(this.gotData.bind(this));
 }
