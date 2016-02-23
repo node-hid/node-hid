6b91c195e2f76b8fe2c244e1adb784e05b6113c2
Tod E. Kurt <tod@todbot.com>
2016-02-22 23:23:02 -0800

    added test-blink1.js example for to show blink(1) USB LED use

---
+++ src/test-blink1.js
@@ -0,0 +1,57 @@
+/**
+ * Simple demostration of sendFeatureReport 
+ * for a blink(1) USB LED by ThingM http://blink1.thingm.com/
+ */
+
+var HID = require('../');
+
+var VENDOR_ID = 0x27B8;
+var PRODUCT_ID = 0x01ED;
+var REPORT_ID = 1;
+// var REPORT_LENGTH = 9;
+
+var devices_found = HID.devices( VENDOR_ID, PRODUCT_ID );
+
+if( devices_found.length === 0 ) {
+    console.log("no blink(1) devices found");
+    process.exit(0);
+}
+console.log("blink(1) devices found:", devices_found);
+
+var hidDevice = new HID.HID( VENDOR_ID, PRODUCT_ID );
+
+hidDevice.on('data', function(data) {
+    console.log("got data");
+});
+
+var blink1_sendCommand = function( /* command [, args ...]*/ ) {
+    var featureReport = [REPORT_ID, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    featureReport[1] = arguments[0].charCodeAt(0);
+
+    for (var i = 1; i < arguments.length; i++) {
+        featureReport[i + 1] = arguments[i];
+    }
+    hidDevice.sendFeatureReport(featureReport);
+};
+var blink1_fadeToColor = function( fadeMillis, r,g,b, ledn ) {
+    var dms = fadeMillis / 10;
+    blink1_sendCommand( 'c', r,g,b, dms>>8, dms%0xff, ledn );
+};
+
+setTimeout( function() {
+    console.log("Setting blink(1) to #00FF33 over 300 millisecs");
+    blink1_fadeToColor( 300, 0x00,0xFF,0x33, 0);
+}, 100);
+
+setTimeout( function() {
+    console.log("Setting blink(1) to #FF6633 over 700 millisecs");
+    blink1_fadeToColor( 700, 0xff,0x66,0x00, 0);
+}, 1000);
+
+setTimeout( function() {
+    console.log("Setting blink(1) to #000000 over 1000 millisecs, and closing");
+    blink1_fadeToColor( 1000, 0x00,0x00,0x00, 0);
+    hidDevice.close();
+
+}, 2000 );
