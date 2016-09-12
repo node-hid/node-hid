a7d348b3fadf08e58b2e43d336879213d9a969f2
Tod E. Kurt <tod@todbot.com>
2016-09-12 11:57:06 -0700

    added version readback in test-blink1

--- src/test-blink1.js
+++ src/test-blink1.js
@@ -8,7 +8,7 @@ var HID = require('../');
 var VENDOR_ID = 0x27B8;
 var PRODUCT_ID = 0x01ED;
 var REPORT_ID = 1;
-// var REPORT_LENGTH = 9;
+var REPORT_LENGTH = 9;
 
 var devices_found = HID.devices( VENDOR_ID, PRODUCT_ID );
 
@@ -36,10 +36,26 @@ var blink1_sendCommand = function( /* command [, args ...]*/ ) {
     }
     hidDevice.sendFeatureReport(featureReport);
 };
+var blink1_readResponse = function(callback) {
+    callback.apply(null, [hidDevice.getFeatureReport(REPORT_ID, REPORT_LENGTH)]);
+};
+
+var blink1_getVersion = function(callback) {
+    blink1_sendCommand('v');
+    blink1_readResponse(function(response) {
+        var version = String.fromCharCode(response[3]) + '.' + String.fromCharCode(response[4]);
+        callback(version);
+  });
+};
+
 var blink1_fadeToColor = function( fadeMillis, r,g,b, ledn ) {
     var dms = fadeMillis / 10;
     blink1_sendCommand( 'c', r,g,b, dms>>8, dms%0xff, ledn );
 };
+
+blink1_getVersion( function(version) {
+    console.log("blink(1) version: ", version);
+});
 
 setTimeout( function() {
     console.log("Setting blink(1) to #00FF33 over 300 millisecs");
@@ -55,5 +71,4 @@ setTimeout( function() {
     console.log("Setting blink(1) to #000000 over 1000 millisecs, and closing");
     blink1_fadeToColor( 1000, 0x00,0x00,0x00, 0);
     hidDevice.close();
-
 }, 2000 );
