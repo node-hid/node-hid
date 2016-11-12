f0bf30610fa808bd201649d9850db6fc5fd47c58
Tod E. Kurt <tod@todbot.com>
2016-11-12 15:36:49 -0800

    test adding back in usage and usagePage fields to device info

--- src/HID.cc
+++ src/HID.cc
@@ -561,6 +561,8 @@ NAN_METHOD(HID::devices)
     }
     deviceInfo->Set(Nan::New<String>("release").ToLocalChecked(), Nan::New<Integer>(dev->release_number));
     deviceInfo->Set(Nan::New<String>("interface").ToLocalChecked(), Nan::New<Integer>(dev->interface_number));
+    deviceInfo->Set(Nan::New<String>("usagePage").ToLocalChecked(), Nan::New<Integer>(dev->usage_page));
+    deviceInfo->Set(Nan::New<String>("usage").ToLocalChecked(), Nan::New<Integer>(dev->usage));
     retval->Set(count++, deviceInfo);
   }
   hid_free_enumeration(devs);
