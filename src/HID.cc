3b83233bad31aa4aee04e0b93c24b5ea3da6dc05
Tod E. Kurt <tod@todbot.com>
2017-08-14 12:03:13 -0700

    do not kill node process at init/exit, issue #217

--- src/HID.cc
+++ src/HID.cc
@@ -577,8 +577,9 @@ static void
 deinitialize(void*)
 {
   if (hid_exit()) {
-    cerr << "cannot initialize hidapi (hid_init failed)" << endl;
-    abort();
+    cerr << "cannot uninitialize hidapi (hid_exit failed)" << endl;
+    return Nan::ThrowTypeError("cannot uninitialize hidapi (hid_exit failed)");
+    // abort();
   }
 }
 
@@ -587,7 +588,8 @@ HID::Initialize(Local<Object> target)
 {
   if (hid_init()) {
     cerr << "cannot initialize hidapi (hid_init failed)" << endl;
-    abort();
+    return Nan::ThrowTypeError("cannot initialize hidapi (hid_init failed)");
+    //abort();
   }
 
   node::AtExit(deinitialize, 0);
