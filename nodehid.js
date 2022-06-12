4c7a258df4dc00b61d04265b39062a3451302028
Julian Waller <git@julusian.co.uk>
2022-06-12 12:49:46 +0100

    fix: revert changes to HID api

--- nodehid.js
+++ nodehid.js
@@ -35,9 +35,7 @@ function HID() {
     /* We also want to inherit from `binding.HID`, but unfortunately,
         it's not so easy for native Objects. For example, the
         following won't work since `new` keyword isn't used:
-
         `binding.HID.apply(this, arguments);`
-
         So... we do this craziness instead...
     */
     var thisPlusArgs = new Array(arguments.length + 1);
@@ -56,19 +54,15 @@ function HID() {
         this[i] = binding.HID.prototype[i].bind(this._raw);
 
     /* We are now done inheriting from `binding.HID` and EventEmitter.
-
         Now upon adding a new listener for "data" events, we start
         polling the HID device using `read(...)`
         See `resume()` for more details. */
+    this._paused = true;
     var self = this;
     self.on("newListener", function(eventName, listener) {
         if(eventName == "data")
             process.nextTick(self.resume.bind(self) );
     });
-    self.on("removeListener", function(eventName, listener) {
-        if(eventName == "data" && self.listenerCount("data") == 0)
-            process.nextTick(self.pause.bind(self) );
-    })
 }
 //Inherit prototype methods
 util.inherits(HID, EventEmitter);
@@ -82,23 +76,44 @@ HID.prototype.close = function close() {
 };
 //Pauses the reader, which stops "data" events from being emitted
 HID.prototype.pause = function pause() {
-    this._raw.readStop();
+    this._paused = true;
+};
+
+HID.prototype.read = function read(callback) {
+    if (this._closed) {
+    throw new Error('Unable to read from a closed HID device');
+  } else {
+    return this._raw.read(callback);
+  }
 };
 
 HID.prototype.resume = function resume() {
     var self = this;
-    if(self.listenerCount("data") > 0)
+    if(self._paused && self.listeners("data").length > 0)
     {
         //Start polling & reading loop
-        self._raw.readStart(function (err, data) {
-            if (err) {
+        self._paused = false;
+        self.read(function readFunc(err, data) {
+            if(err)
+            {
+                //Emit error and pause reading
+                self._paused = true;
                 if(!self._closing)
                     self.emit("error", err);
                 //else ignore any errors if I'm closing the device
-            } else {
+            }
+            else
+            {
+                //If there are no "data" listeners, we pause
+                if(self.listeners("data").length <= 0)
+                    self._paused = true;
+                //Keep reading if we aren't paused
+                if(!self._paused)
+                    self.read(readFunc);
+                //Now emit the event
                 self.emit("data", data);
             }
-        }, self)
+        });
     }
 };
 
